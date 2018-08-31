f66f18336f4d72f735daf25bdc229793acc843fb
hidebe9203 <kskhidebe9203@gmail.com>
2018-08-31 19:33:16 +0900

    Refactoring
    以下をリファクタリング
    ・message.js
    ・_message.html.haml
    ・create.json.jbuilder
    リファクタリングに伴いCSSを変更
    ・_index.scss

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,11 @@
 $(function() {
 
   function buildHTML(comment) {
-    if (comment.image !== null) {
-      var imageUrl = `<img src=${ comment.image } >`
-    } else {
-      var imageUrl = ''
-    }
+    var imageUrl = (comment.image_url !== null) ? `<img src=${ comment.image_url } >` : '';
     var html = `<div class="main__contents__content">
                   <div class="main__contents__content__title">
-                    <h2>${ comment.name }<span>${ comment.created_at }</span></h2>
+                    ${ comment.user_name }
+                    <span>${ comment.created_at }</span>
                   </div>
                   <div class="main__contents__content__body">
                     <p>${ comment.content }<p>
