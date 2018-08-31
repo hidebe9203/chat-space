a534d05d6b8dfec095ed0896a7b56077440ebb62
hidebe9203 <kskhidebe9203@gmail.com>
2018-08-31 18:54:32 +0900

    Support for asynchronous communication
    メッセージ投稿機能をAjax化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,11 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    else
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+        format.json
+      end
     end
   end
 
